80da7c714469ca78832dd649491945fc7afb2edc
PrestonSo4 <75041856+PrestonSo4@users.noreply.github.com>
2021-06-11 22:49:15 +0000

    Simplified

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from letters import *
 
-letters_list = {'a':a,'b':b, 'c':c, 'd':d, 'e':e,'f':f, 'g':g, 'h':h, 'i':i,'j':j,'k':k,'l':l,'m':m,'n':n,'o':o,'p':p, ' ':space, '.':period, ':':colon}
+letters_list = {'a':a,'b':b, 'c':c, 'd':d, 'e':e,'f':f, 'g':g, 'h':h, 'i':i,'j':j,'k':k,'l':l,'m':m,'n':n,'o':o,'p':p,'q':q,'r':r,'s':s, ' ':space, '.':period, ':':colon, '-':hyphen}
 
 output1 = []
 output2 = []
@@ -9,32 +9,15 @@ output4 = []
 output5 = []
 output6 = []
 def main():
-    text = input('Input text: ').lower()
-    for letters in text:    
-        for i in range(6):
-            var = letters_list.get(letters)
-            if i == 0:
-                output1.append(var[i][0])
-            elif i == 1:
-                output2.append(var[i][0])
-            elif i == 2:
-                output3.append(var[i][0])
-            elif i == 3:
-                output4.append(var[i][0])
-            elif i == 4:
-                output5.append(var[i][0])
-            elif i == 5:
-                output6.append(var[i][0])
-    output()
-def output():
-    print(*output1, '\n',*output2,'\n',*output3,'\n',*output4,'\n',*output5,'\n',*output6,sep='')
-    output1.clear()
-    output2.clear()
-    output3.clear()
-    output4.clear()
-    output5.clear()
-    output6.clear()
+	text = input('Enter text: ')
+	for rows in range(len(space)):
+		string = ''
+		for chars in text:
+			var = letters_list.get(chars)
+			string += var[rows][0]
+		print(string)
     
 if __name__ == '__main__':
     while True:
         main()
+
